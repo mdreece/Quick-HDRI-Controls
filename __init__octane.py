25605f286de500b8d8a520657910c569f4dae7e9
mdreece <87138240+mdreece@users.noreply.github.com>
2025-07-14 20:56:13 -0500

    Update __init__octane.py

--- __init__octane.py
+++ __init__octane.py
@@ -21,7 +21,7 @@ import numpy as np
 bl_info = {
     "name": "Quick HDRI Controls (Octane)",
     "author": "Dave Nectariad Rome",
-    "version": (2, 8, 8),
+    "version": (2, 8, 9),
     "blender": (4, 0, 0),
     "location": "3D Viewport > Header",
     "warning": "Alpha Version (in-development)",

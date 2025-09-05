4e25bb3fd4a85b5ecee89735e8b755e2dec5b82b
mdreece <87138240+mdreece@users.noreply.github.com>
2025-09-05 15:00:40 -0500

    Update __init__cycles.py

--- __init__cycles.py
+++ __init__cycles.py
@@ -21,7 +21,7 @@ import numpy as np
 bl_info = {
     "name": "Quick HDRI Controls (Cycles)",
     "author": "Dave Nectariad Rome",
-    "version": (2, 8, 9),
+    "version": (2, 9, 0),
     "blender": (4, 0, 0),
     "location": "3D Viewport > Header",
     "warning": "Alpha Version (in-development)",
@@ -4940,3 +4940,4 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+

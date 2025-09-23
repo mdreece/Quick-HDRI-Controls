624fab7ae41026de1223060c4ce19c02780db347
mdreece <87138240+mdreece@users.noreply.github.com>
2025-09-23 17:06:01 -0500

    Update __init__vray.py

--- __init__vray.py
+++ __init__vray.py
@@ -22,7 +22,7 @@ import numpy as np
 bl_info = {
     "name": "Quick HDRI Controls (V-Ray)",
     "author": "Dave Nectariad Rome",
-    "version": (2, 9, 2),
+    "version": (2, 9, 3),
     "blender": (4, 0, 0),
     "location": "3D Viewport > Header",
     "warning": "Alpha Version (in-development)",
@@ -4998,6 +4998,7 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+
 
 
 
